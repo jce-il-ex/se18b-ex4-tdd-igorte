46bae8681741a7391536f8e971fd0f900fd9431a
igorte <xigalx@gmail.com>
2018-06-03 01:55:25 +0300

    GREEN: added more test to make-it-pass.test and succesfuly passed

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -4,8 +4,16 @@ function sum (a, b) {
 	return a+b;
   // your code to make the test pass goes here ...
 }
+function sub (a, b) {
+	return a-b;
+}
 
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+
+test('sub should return the substraction of two numbers', function (t) {
+  t.equal(3, sub(5, 2)); 
   t.end();
 });
